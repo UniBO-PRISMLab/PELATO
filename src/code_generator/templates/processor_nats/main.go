dec4a6db661ef886c1d72f537eadf5c3b7587910
Lorenzo Venerandi <lollo.venerandi@gmail.com>
2025-08-10 18:28:13 +0200

    improved logging

--- src/code_generator/templates/processor_nats/main.go
+++ src/code_generator/templates/processor_nats/main.go
@@ -2,6 +2,9 @@
 package main
 
 import (
+	"fmt"
+	"runtime/debug"
+	"time"
 	logger "github.com/UniBO-PRISMLab/PELATO/src/code_generator/templates/processor_nats/gen/wasi/logging/logging"
 	"github.com/UniBO-PRISMLab/PELATO/src/code_generator/templates/processor_nats/gen/wasmcloud/messaging/consumer"
 	"github.com/UniBO-PRISMLab/PELATO/src/code_generator/templates/processor_nats/gen/wasmcloud/messaging/handler"
@@ -9,49 +12,68 @@ import (
 	"github.com/bytecodealliance/wasm-tools-go/cm"
 )
 
+// NOTE: helper logging
+func logf(level logger.Level, msg string, kv ...string) {
+	if len(kv) > 0 {
+		msg = msg + " | " + fmt.Sprint(kv)
+	}
+	logger.Log(level, "MessageHandler", msg)
+}
+
 type messagingConsumerAdapter struct {
 	Publish func(msg types.BrokerMessage) (result cm.Result[string, struct{}, string])
 }
 
-// NOTE(lxf): this is overridden in tests
-var messagingConsumer = &messagingConsumerAdapter{
-	Publish: consumer.Publish,
-} 
+var messagingConsumer = &messagingConsumerAdapter{Publish: consumer.Publish}
 
-func init() {
-	handler.Exports.HandleMessage = handleMessage
+func init() { handler.Exports.HandleMessage = handleMessage }
+
+// Wrap exec_task with panic recovery
+func safeExec(input string) (out string, failed bool) {
+	start := time.Now()
+	defer func() {
+		if r := recover(); r != nil {
+			failed = true
+			logf(logger.LevelError, fmt.Sprintf("panic in task: %v", r), "duration", time.Since(start).String(), "stack", string(debug.Stack()))
+		}
+	}()
+	out = exec_task(input) // provided by template/user
+	logf(logger.LevelDebug, "task executed", "duration", time.Since(start).String())
+	return
 }
 
 func handleMessage(msg types.BrokerMessage) cm.Result[string, struct{}, string] {
+	start := time.Now()
+	logf(logger.LevelInfo, "received message", "subject", msg.Subject, "len", fmt.Sprintf("%d", msg.Body.Len()))
 
-	logger.Log(logger.LevelInfo,"MessageHandler", "Received message on subject" + msg.Subject)
-	
-	// TODO implement the logic to get the destination topic from the config
-	// dest_topic := config.GetAll()
-	dest_topic := "{{ dest_topic }}"
+	if msg.Body.Len() == 0 {
+		logf(logger.LevelWarn, "empty body - skipping")
+		return cm.Err[cm.Result[string, struct{}, string]]("empty body")
+	}
 
-	// TASK
+	destTopic := "{{ dest_topic }}" // TODO: load from config
+
 	arg := cm.LiftString[string, *uint8, uint8](msg.Body.Data(), uint8(msg.Body.Len()))
+	result, failed := safeExec(arg)
+	if failed {
+		return cm.Err[cm.Result[string, struct{}, string]]("task panic")
+	}
 
-	result := exec_task(arg)
-
-	// Send reply
 	reply := types.BrokerMessage{
-		Subject: dest_topic,
+		Subject: destTopic,
 		Body:    cm.ToList([]byte(result)),
 		ReplyTo: cm.None[string](),
 	}
 
-	res := messagingConsumer.Publish(reply)
-	if res.IsErr() {
-		logger.Log(logger.LevelError, "MessageHandler", "Failed to send reply, error: " + *res.Err())
-		return res
+	pubRes := messagingConsumer.Publish(reply)
+	if pubRes.IsErr() {
+		errMsg := *pubRes.Err()
+		logf(logger.LevelError, "publish failed", "error", errMsg, "duration", time.Since(start).String())
+		return cm.Err[cm.Result[string, struct{}, string]]("publish failed: " + errMsg)
 	}
 
+	logf(logger.LevelInfo, "message processed", "duration", time.Since(start).String())
 	return cm.OK[cm.Result[string, struct{}, string]](struct{}{})
 }
 
-
-// Since we don't run this program like a CLI, the `main` function is empty. Instead,
-// we call the `handleRequest` function when an HTTP request is received.
 func main() {}
